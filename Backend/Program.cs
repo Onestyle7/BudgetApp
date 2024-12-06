79f5d0dc6b79932bdc6f246fc6e559f6310e6633
Onestyle7 <klusbartek8@gmail.com>
2024-12-06 18:20:19 +0100

    Skonfigurowanie swaggera pod autoryzacje JWT

--- Backend/Program.cs
+++ Backend/Program.cs
@@ -6,6 +6,7 @@ using Microsoft.IdentityModel.Tokens;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using System.Text;
 using Backend.Services;
+using Microsoft.OpenApi.Models;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -35,7 +36,33 @@ builder.Services.AddAuthentication(options =>{
 });
 
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen( c=>{
+    // Dodanie definicji bezpieczeństwa dla JWT w Swaggerze
+c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+{
+    Description = "JWT Authorization header using the Bearer scheme",
+    Name = "Authorization", // Nazwa nagłówka, który będzie zawierał token JWT
+    In = ParameterLocation.Header, // Token będzie przekazywany w nagłówku HTTP
+    Type = SecuritySchemeType.ApiKey, // Typ schematu bezpieczeństwa
+    Scheme = "Bearer" // Nazwa schematu
+});
+
+// Dodanie wymagań bezpieczeństwa dla Swaggera, aby każdy endpoint wymagał tokenu JWT
+c.AddSecurityRequirement(new OpenApiSecurityRequirement
+{
+    {
+        new OpenApiSecurityScheme
+        {
+            Reference = new OpenApiReference
+            {
+                Type = ReferenceType.SecurityScheme, // Odniesienie do schematu bezpieczeństwa
+                Id = "Bearer" // Nazwa schematu zdefiniowana powyżej
+            }
+        },
+        new string[] {} // Puste stringi oznaczają brak dodatkowych wymagań
+    }
+});
+});
 builder.Services.AddCors(options =>{
     options.AddPolicy("AllowFrontend",
     policy => policy.WithOrigins("http://localhost:8080")
@@ -43,6 +70,13 @@ builder.Services.AddCors(options =>{
     .AllowAnyHeader());
 });
 builder.Services.AddScoped<IUserService, UserService>();
+// Rejestracja serwisu dla obsługi transakcji w kontenerze DI
+builder.Services.AddScoped<ITransactionService, TransactionService>();
+
+// Rejestracja AutoMappera, aby mógł automatycznie mapować DTO na modele i odwrotnie
+builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
+
+
 builder.Services.AddControllers();
 
 var app = builder.Build();
