0952eb95e67eb23f6c95fd2679d765d82e087757
Onestyle7 <klusbartek8@gmail.com>
2024-12-08 22:21:51 +0100

    Modyfikacja i uzupełnienie brakujących metod do CRUD

--- Backend/Services/TransactionService.cs
+++ Backend/Services/TransactionService.cs
@@ -4,6 +4,8 @@ using System.Linq;
 using System.Threading.Tasks;
 using Backend.Models; // Używamy modelu Transaction z Backend.Models
 using Backend.Data;
+using Microsoft.EntityFrameworkCore;
+using System.Security.Claims;
 
 namespace Backend.Services
 {
@@ -33,26 +35,62 @@ namespace Backend.Services
             // Właściwość Entity zawiera oryginalny obiekt transakcji, który został dodany.
             return transac.Entity; // Zwracamy transakcję z właściwości Entity
         }
-
-        public Task<Transactions> DeleteTransactionAsync(int id)
+        // Usuwanie transakcji przypisanej do konkretnego użytkownika
+        public async Task DeleteTransactionAsync(int id, int userId)
         {
-            throw new NotImplementedException();
+            // Znajduje transakcję na podstawie ID i ID użytkownika
+            var transaction = await _context.Transactions
+                .FirstOrDefaultAsync(t => t.Id == id && t.UserId == userId);
+
+            // Sprawdza, czy transakcja istnieje i czy użytkownik ma uprawnienia do jej usunięcia
+            if (transaction == null)
+            {
+                throw new UnauthorizedAccessException("TTransaction not found or you are not allowed to delete it");
+            }
+
+            // Usuwa transakcję z bazy danych
+            _context.Transactions.Remove(transaction);
+            await _context.SaveChangesAsync();
         }
-
-        public Task<IEnumerable<Transactions>> GetAllTransactionsAsync()
+        // Pobieranie wszystkich transakcji przypisanych do konkretnego użytkownika
+        public async Task<IEnumerable<Transactions>> GetAllTransactionsAsync(int userId)
         {
-            throw new NotImplementedException();
+            // Filtruje transakcje w bazie danych na podstawie ID użytkownika i dołącza dane użytkownika
+            return await _context.Transactions
+            .Where(t => t.UserId == userId)
+            .Include(t => t.User)
+            .ToListAsync();
         }
-
-        public Task<Transactions> GetTransactionByIdAsync(int id)
+        // Pobieranie transakcji na podstawie ID (bez sprawdzania użytkownika)
+        public Task<Transactions?> GetTransactionByIdAsync(int id)
         {
+            // Znajduje transakcję w bazie danych na podstawie ID
             var transaction = _context.Transactions.FirstOrDefault(t => t.Id == id);
             return Task.FromResult(transaction);
         }
-
-        public Task<Transactions> UpdateTransactionAsync(int id, Transactions transaction)
+        // Aktualizowanie transakcji przypisanej do konkretnego użytkownika
+        public async Task<Transactions> UpdateTransactionAsync(int id, Transactions transaction, int userId)
         {
-            throw new NotImplementedException();
+            // Znajdujemy istniejącą transakcję
+            var existingTransaction = await _context.Transactions
+                .FirstOrDefaultAsync(t => t.Id == id && t.UserId == userId);
+                
+            // Sprawdza, czy transakcja istnieje i czy użytkownik ma uprawnienia do jej aktualizacji
+            if (existingTransaction == null)
+            {
+                throw new UnauthorizedAccessException("Transaction not found or you are not allowed to update it");
+            }
+
+            // Aktualizujemy pola transakcji
+            existingTransaction.Amount = transaction.Amount;
+            existingTransaction.Category = transaction.Category;
+            existingTransaction.Date = transaction.Date;
+
+            // Zapisujemy zmiany
+            await _context.SaveChangesAsync();
+
+            return existingTransaction;
         }
+
     }
 }
