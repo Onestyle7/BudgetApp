6e6491e92f9f4e5594aa37efa58e87e47b31c6ae
Onestyle7 <klusbartek8@gmail.com>
2024-12-08 22:20:47 +0100

    Dodanie przypisania każdemu enum wartości liczbowej

--- Backend/Models/Enums/Category.cs
+++ Backend/Models/Enums/Category.cs
@@ -2,13 +2,13 @@ namespace Backend.Models.Enums
 {
     public enum Category
     {
-        Subscriptions,
-        Groceries,
-        Rent,
-        Utilities,
-        Transportation,
-        Health,
-        Entertainment,
-        Other
+        Subscriptions = 1,
+        Groceries = 2,
+        Rent = 3,
+        Utilities = 4,
+        Transportation = 5,
+        Health = 6,
+        Entertainment = 7,
+        Other = 8
     }
 }
