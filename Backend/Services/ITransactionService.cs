d116b6f8d9bd4f4d9205389e8edd3bee0c854fdb
Onestyle7 <klusbartek8@gmail.com>
2024-12-08 22:21:07 +0100

    Edycja przyjmowanych parametrów przez metody

--- Backend/Services/ITransactionService.cs
+++ Backend/Services/ITransactionService.cs
@@ -10,10 +10,10 @@ namespace Backend.Services
 public interface ITransactionService
 {
     Task<Transactions> AddTransactionAsync(Transactions transaction); // Dodawanie transakcji
-    Task<IEnumerable<Transactions>> GetAllTransactionsAsync(); // Pobranie wszystkich transakcji
-    Task<Transactions> GetTransactionByIdAsync(int id); // Pobranie transakcji po ID
-    Task<Transactions> UpdateTransactionAsync(int id, Transactions transaction); // Aktualizacja transakcji
-    Task<Transactions> DeleteTransactionAsync(int id); // Usunięcie transakcji
+    Task<IEnumerable<Transactions>> GetAllTransactionsAsync(int userId); // Pobranie wszystkich transakcji
+    Task<Transactions?> GetTransactionByIdAsync(int id); // Pobranie transakcji po ID
+    Task<Transactions> UpdateTransactionAsync(int id, Transactions transaction, int userId); // Aktualizacja transakcji
+    Task DeleteTransactionAsync(int id, int userId); // Usunięcie transakcji
 }
 
 }
